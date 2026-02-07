c2cdf573619d678d387be27e5e232c11161f1920
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-02-08 01:26:03 +0530

    refactor: make the directory properly for long term use

---
+++ Tutorial/hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args) {
+        // Println will help you print and go to next line while print only prints
+        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
